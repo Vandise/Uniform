3fe1726b9902c0e317eb6c49ca98f1ac965d05a1
Benjamin Anderson <andeb2804@gmail.com>
2024-09-04 13:39:23 -0500

    Begin scanner initialization

--- modules/scanner/inc/shared.h
+++ modules/scanner/inc/shared.h
@@ -1,4 +1,54 @@
 #ifndef __UNIFORM_SCANNER_SHAREDH
 #define __UNIFORM_SCANNER_SHAREDH 1
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define EOF_CHAR     '\x7f'
+#define NEWLINE_CHAR '\n'
+#define COMMENT_CHAR '#'
+#define UNIFORM_SCANNER_VERSION 100
+
+typedef enum {
+  LETTER,
+  UPPERCASE_LETTER,
+  DIGIT,
+  QUOTE,
+  UNDERSCORE,
+  MACRO,
+  SPECIAL,
+  NEWLINE,
+  EOF_CODE
+} CHAR_CODE;
+
+typedef struct UniformScannerStruct {
+  unsigned int line_number;
+  unsigned int level;
+  unsigned int buffer_offset;
+  unsigned int digit_count;
+
+  CHAR_CODE char_table[256];
+
+  char source_buffer[1024];
+  char source_name[64];
+  FILE *source_file;
+
+  char *source_bufferp;
+  char current_char;
+
+  unsigned int errored;
+
+} UniformScanner;
+
+struct uniform_scanner_module {
+  const signed int version;
+
+  UniformScanner* (*init)(const char*);
+  void (*close)(UniformScanner*);
+};
+
+extern const struct uniform_scanner_module UniformScannerModule;
+
 #endif
--- modules/scanner/src/scanner.c
+++ modules/scanner/src/scanner.c
@@ -1,1 +1,62 @@
-int SCANNER_VERSION = 110;
+#include "uniform/scanner/shared.h"
+
+// ============================
+//          Forwards
+// ============================
+
+static UniformScanner* init(const char *file_name);
+static int open_source_file(UniformScanner *scanner);
+
+// ============================
+//        Implementation
+// ============================
+
+static UniformScanner* init(const char *file_name) {
+  errno = 0;
+
+  UniformScanner* scanner = malloc(sizeof(UniformScanner));
+  strcpy(scanner->source_name, file_name);
+  strcpy(scanner->source_buffer, "");
+
+  scanner->current_char = '\0';
+  scanner->source_bufferp = NULL;
+  scanner->line_number = 0;
+  scanner->level = 0;
+  scanner->buffer_offset = 0;
+  scanner->errored = 0;
+
+  if (open_source_file(scanner) == 0) {
+    scanner->errored = 1;
+  }
+
+  return scanner;
+}
+
+static int open_source_file(UniformScanner *scanner) {
+  scanner->source_file = fopen(scanner->source_name, "r");
+
+  if (errno != 0) { return 0; }
+
+  scanner->source_bufferp = scanner->source_buffer;
+
+  return 1;
+}
+
+static void close(UniformScanner *scanner) {
+  if (scanner->source_file != NULL) {
+    fclose(scanner->source_file);
+    scanner->source_file = NULL;
+  }
+
+  free(scanner);
+}
+
+// ============================
+//            Module
+// ============================
+
+const struct uniform_scanner_module UniformScannerModule = {
+  .version = UNIFORM_SCANNER_VERSION,
+  .init = init,
+  .close = close
+};
