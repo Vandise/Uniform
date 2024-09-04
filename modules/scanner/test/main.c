0f847a8b1e36ea19e788c2d0fdb18ff4c8e74e08
Benjamin Anderson <andeb2804@gmail.com>
2024-09-04 14:15:13 -0500

    Begin scanner init tests

--- modules/scanner/test/main.c
+++ modules/scanner/test/main.c
@@ -1,8 +1,27 @@
 #include "awry/awry.h"
+#include "uniform/scanner/shared.h"
 
 describe("Scanner Test Suite", scanner_test_suite)
   it("is instrumented properly")
     expect(0) to equal(0)
+  end
+
+  context(".init")
+    when("A file is not found")
+      UniformScanner *scanner = UniformScannerModule.init("test/files/notfound.u");
+  
+      it("Sets the scanner's errored status to 1")
+        expect(scanner->errored) to equal(1)
+      end
+    end
+
+    when("A file is found")
+      UniformScanner *scanner = UniformScannerModule.init("modules/scanner/test/files/tokens.u");
+  
+      it("Sets the scanner's errored status to 0")
+        expect(scanner->errored) to equal(0)
+      end   
+    end
   end
 end
 
