ab8315c8299ae3344fcd790e5ca8365f0818b449
Benjamin Anderson <andeb2804@gmail.com>
2024-10-03 16:53:10 -0500

    swap modules and functions tests

--- modules/parser/test/modules_test.c
+++ modules/parser/test/modules_test.c
@@ -17,34 +17,54 @@
   #define SCANNER_LIB "lib/libuniformscanner.dylib.1.0.0"
 #endif
 
-UniformSymbolTable* functions_table = NULL;
-UniformPreprocessor* functions_preprocessor = NULL;
-UniformParser* functions_parser = NULL;
+UniformSymbolTable* modules_table = NULL;
+UniformPreprocessor* modules_preprocessor = NULL;
+UniformParser* modules_parser = NULL;
 
-define_fixture(before, before_functions) {
-  functions_table = UniformSymbolTableModule.init();
-  UniformCoreIntegerModule.init(functions_table);
+define_fixture(before, before_modules) {
+  modules_table = UniformSymbolTableModule.init();
+  UniformCoreIntegerModule.init(modules_table);
 
-  functions_preprocessor = UniformPreprocessorModule.init(SCANNER_LIB, 0);
-  UniformPreprocessorModule.process(functions_preprocessor, "modules/parser/test/files/functions.u", NULL);
+  modules_preprocessor = UniformPreprocessorModule.init(SCANNER_LIB, 0);
+  UniformPreprocessorModule.process(modules_preprocessor, "modules/parser/test/files/modules.u", NULL);
 
-  functions_parser = UniformParserModule.init(functions_table, functions_preprocessor->token_array);
+  modules_parser = UniformParserModule.init(modules_table, modules_preprocessor->token_array);
 }
 
-define_fixture(after, after_functions) {
-  UniformParserModule.close(functions_parser);
-  UniformPreprocessorModule.close(functions_preprocessor);
-  UniformSymbolTableModule.clear(functions_table);
+define_fixture(after, after_modules) {
+  UniformParserModule.close(modules_parser);
+  UniformPreprocessorModule.close(modules_preprocessor);
+  UniformSymbolTableModule.clear(modules_table);
 }
 
-describe("Functions Parser Test Suite", functions_parser_test_suite)
-  before(before_functions)
-  after(after_functions)
+describe("Modules Parser Test Suite", modules_parser_test_suite)
+  before(before_modules)
+  after(after_modules)
 
-  context("function declarations")
-    it("processes functions with no arguments and a return type")
-      UniformASTNode* node = UniformModuleParser.process(functions_parser, NULL);
+  context(".modules")
+    it("processes modules")
+      UniformASTNode* node = UniformModuleParser.process(modules_parser, NULL);
       UniformASTModuleNode* module = (UniformASTModuleNode*)(node->data);
+
+      expect(module->identifier) to equal("Main")
+
+      UniformASTBodyNode* body = module->body;
+      UniformASTModuleNode* submodule = (UniformASTModuleNode*)(body->nodes[0]->data);
+
+      expect(submodule->identifier) to equal("SubMain")
+    end
+
+    it("adds the symbols to the symbol table")
+      UniformASTNode* node = UniformModuleParser.process(modules_parser, NULL);
+      UniformSymbolTableNode* mainsym = UniformSymbolTableModule.search_global(modules_table, "Main");
+
+      expect(mainsym->name) to equal("Main")
+
+      UniformSymbolTableNode* subsym = UniformSymbolTableModule.search_global(mainsym->definition.info.module.symbol_table, "SubMain");
+      expect(subsym->name) to equal("SubMain")
+
+      UniformSymbolTableNode* declsym = UniformSymbolTableModule.search_global(subsym->definition.info.module.symbol_table, "R");
+      expect(declsym->name) to equal("R")
     end
   end
 end
